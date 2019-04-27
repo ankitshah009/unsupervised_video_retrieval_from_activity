a6c7b155e97686755397ae65eb93b5995113abca
Rawal Khirodkar <rawalkhirodkar@gmail.com>
2019-04-27 14:22:46 -0400

    Add preprocessing scripts, training code for pytorch 1.0

---
+++ scripts/convert.sh
@@ -0,0 +1,13 @@
+cd ..
+
+# ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+## HMDB
+# python utils/video_jpg_ucf101_hmdb51.py ./data/raw_data/hmdb ./data/raw_data/hmdb
+# python utils/n_frames_ucf101_hmdb51.py ./data/hmdb
+# python utils/hmdb51_json.py ./data/raw_data/testTrainMulti_7030_splits
+
+# ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+## UCF
+# python utils/video_jpg_ucf101_hmdb51.py ./data/raw_data/UCF-101 ./data/raw_data/UCF-101
