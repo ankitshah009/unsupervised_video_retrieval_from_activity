164f4eb0a10e86c84f303320dd71c44dcf6c1f74
Rawal Khirodkar <rawalkhirodkar@gmail.com>
2019-04-27 22:46:42 -0400

    Delete unnecessary files

---
+++ finetuning/scripts/finetune.sh
@@ -0,0 +1,19 @@
+cd ..
+
+# ----------------------------------------------------------------------------------------------
+# CUDA_VISIBLE_DEVICES=1 python main.py --root_path ~/data --video_path kinetics_videos/jpg --annotation_path kinetics.json \
+# --result_path results --dataset kinetics --model resnet \
+# --model_depth 34 --n_classes 400 --batch_size 128 --n_threads 4 --checkpoint 5
+
+# # ----------------------------------------------------------------------------------------------
+# CUDA_VISIBLE_DEVICES=0 python main.py --root_path ./data --video_path hmdb/jpg --annotation_path hmdb/hmdb51_1.json \
+# --result_path hmdb/results --dataset hmdb51 --model resnet \
+# --model_depth 34 --n_classes 51 --batch_size 128 --n_threads 4 --checkpoint 5
+
+# ----------------------------------------------------------------------------------------------
+CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py --root_path ./../data --video_path hmdb/jpg --annotation_path hmdb/hmdb51_1.json \
+--result_path hmdb/results --dataset hmdb51 --model resnet \
+--model_depth 50 --n_classes 51 --batch_size 64 --n_threads 32 --checkpoint 5 \
+--n_epochs 50 \
+
+# ----------------------------------------------------------------------------------------------
