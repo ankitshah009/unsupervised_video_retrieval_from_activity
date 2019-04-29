5a0101fe8e694e22c6de565ae0f4482f5536014e
Rawal Khirodkar <rawalkhirodkar@gmail.com>
2019-04-28 21:09:17 -0400

    Update

--- scripts/train_ucf.sh
+++ scripts/train_ucf.sh
@@ -2,9 +2,9 @@ cd ..
 
 # ----------------------------------------------------------------------------------------------
 ## use 2x sample duration
-CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py --root_path ./data --video_path hmdb/jpg --annotation_path hmdb/hmdb51_1.json \
---result_path hmdb/results_triplet --dataset hmdb51 --n_classes 400 --n_finetune_classes 51 --model resnext \
---model_depth 101 --resnet_shortcut B --resnext_cardinality 32 --sample_duration 64 --batch_size 16 --pretrain_path kinetics/resnext-101-64f-kinetics.pth \
---ft_begin_index 4 --n_threads 32 --checkpoint 5 --n_epochs 400 \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python main.py --root_path ./data --video_path ucf/jpg --annotation_path ucf/ucf101_01.json \
+--result_path ucf/results_triplet --dataset ucf101 --n_classes 101 --model resnext \
+--model_depth 101 --resnet_shortcut B --resnext_cardinality 32 --sample_duration 64 --batch_size 16 \
+--n_threads 32 --checkpoint 5 --n_epochs 400 \
 
 # ----------------------------------------------------------------------------------------------
