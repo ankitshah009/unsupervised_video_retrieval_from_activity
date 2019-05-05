6e376c605d933f471d5c34a86fb9eeb20a4304e5
Rawal Khirodkar <rawalkhirodkar@gmail.com>
2019-05-05 19:06:25 -0400

    Updating train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -13,4 +13,4 @@ cd ..
 CUDA_VISIBLE_DEVICES=1,2,3 python main.py --root_path ./data --video_path hmdb/jpg --annotation_path hmdb/hmdb51_1.json \
 --result_path hmdb/results_triplet_scratch --dataset hmdb51 --n_classes 51 --model resnext \
 --model_depth 101 --resnet_shortcut B --resnext_cardinality 32 --sample_duration 64 --batch_size 16 \
---n_threads 32 --checkpoint 5 --n_epochs 250 \
+--n_threads 32 --checkpoint 5 --n_epochs 250 --no_val \
